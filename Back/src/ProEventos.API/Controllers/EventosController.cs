5424b7c639f1fc0710f507dfe596af9f90b57913
CleberMunizR <clebermuniz.r@gmail.com>
2022-01-01 19:15:22 -0300

    Seção 4: Angular / Bootstrap

---
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+
+       
+        public EventosController(DataContext Context)
+        {
+            this._context = Context;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(Evento => Evento.EventoID == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de put com id = {id}";
+        }
+    }
+}
